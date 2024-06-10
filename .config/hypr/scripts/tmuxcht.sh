e9b60e32482d4b53ebe7e5a91f69cd1db2c7e9a0
BIJESH <BIIJESH@proton.me>
2024-06-10 14:39:20 +0545

    tmux conf edited

--- .config/hypr/scripts/tmuxcht.sh
+++ .config/hypr/scripts/tmuxcht.sh
@@ -1,12 +1,12 @@
 
-selected=`cat ~/.tmux-cht-languages ~/.tmux-cht-command | fzf`
+selected=`cat ~/tmux/.tmux-cht-languages ~/.tmux/tmux-cht-command | fzf`
 if [[ -z $selected ]]; then
     exit 0
 fi
 
 read -p "Enter Query: " query
 
-if grep -qs "$selected" ~/.tmux-cht-languages; then
+if grep -qs "$selected" ~/tmux/.tmux-cht-languages; then
     query=`echo $query | tr ' ' '+'`
     tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
 else
