53754c9904fe66f7bb81d8007f93fb7ea9f6d198
BIJESH <BIIJESH@proton.me>
2024-06-17 13:41:58 +0545

    resize windows

--- .config/nvim/lua/plugins/util.lua
+++ .config/nvim/lua/plugins/util.lua
@@ -27,15 +27,5 @@ return {
 			},
 		},
 	},
-	{
-		"folke/zen-mode.nvim",
-		cmd = "ZenMode",
-		dependencies = { "folke/twilight.nvim" },
-		opts = {},
-		keys = {
-			{ "<leader>tz", "<cmd>ZenMode<CR>", desc = "Zen mode" },
-		},
-	},
-
 
 }
