54a201a9f479ac2b74377ea3930d1038c051cfe3
BIJESH <BIIJESH@proton.me>
2024-06-20 09:51:32 +0545

    better panes in nvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
+local opts = { silent = true }
+vim.api.nvim_set_keymap('n', '<C-h>', ':wincmd h<CR>', opts)
+vim.api.nvim_set_keymap('n', '<C-l>', ':wincmd l<CR>', opts)
 vim.keymap.set("n", "<Esc>", "<cmd>noh<CR>", { desc = "No highlight" })
 vim.keymap.set("n", "<leader>bd", "<cmd>bd<CR>", { desc = "Delete buffer" })
 vim.keymap.set("n", "<leader>bD", "<cmd>bd!<CR>", { desc = "Delete buffer (Force)" })
