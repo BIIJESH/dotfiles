68d8cb4ac2ca786d739129906af025315eddd793
BIJESH <BIIJESH@proton.me>
2024-07-08 09:18:41 +0545

    syntax highlighting in fshow

--- tmux/fshow.sh
+++ tmux/fshow.sh
@@ -1,15 +1,16 @@
 function fshow() {
   git log --graph --color=always \
-      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
-  fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort --preview \
-         'f() { set -- $(echo -- "$@" | grep -o "[a-f0-9]\{7\}"); [ $# -eq 0 ] || git show --color=always $1 ; }; f {}' \
-      --header "enter to view, ctrl-o to checkout" \
-      --bind "q:abort,ctrl-f:preview-page-down,ctrl-b:preview-page-up" \
-      --bind "ctrl-o:become:(echo {} | grep -o '[a-f0-9]\{7\}' | head -1 | xargs git checkout)" \
-      --bind "ctrl-m:execute:
-                (grep -o '[a-f0-9]\{7\}' | head -1 |
-                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
-                {}
+    --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+  fzf --ansi --no-sort --reverse --tiebreak=index --preview \
+    'echo {} | grep -o "[a-f0-9]\{7\}" | xargs -I % sh -c "git show % | bat --color=always --paging=always -l rs"' \
+    --header "Enter to view diff, Ctrl-O to checkout" \
+    --bind "q:abort,ctrl-f:preview-page-down,ctrl-b:preview-page-up" \
+    --bind "ctrl-o:execute:
+      (echo {} | grep -o '[a-f0-9]\{7\}' | head -1 | xargs git checkout && pkill -f fzf)" \
+    --bind "enter:execute:
+      (echo {} | grep -o '[a-f0-9]\{7\}' | head -1 | xargs -I % sh -c 'git diff %^! | bat --paging=always --color=always -l rs') << 'FZF-EOF'
+      {}
 FZF-EOF" --preview-window=right:60%
 }
+
 fshow "$@"
