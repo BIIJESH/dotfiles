7243c368ddb268b883bab543b92494559411153a
BIJESH <BIIJESH@proton.me>
2024-07-09 08:23:03 +0545

    hyprland

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -10,6 +10,18 @@ vim.api.nvim_create_autocmd("RecordingEnter", {
     })
   end,
 })
+-- Hyprlang LSP
+vim.api.nvim_create_autocmd({'BufEnter', 'BufWinEnter'}, {
+		pattern = {"*.hl", "hypr*.conf"},
+		callback = function(event)
+				print(string.format("starting hyprls for %s", vim.inspect(event)))
+				vim.lsp.start {
+						name = "hyprlang",
+						cmd = {"hyprls"},
+						root_dir = vim.fn.getcwd(),
+				}
+		end
+})
 vim.api.nvim_create_autocmd("RecordingLeave", {
   callback = function()
     local timer = vim.loop.new_timer()
